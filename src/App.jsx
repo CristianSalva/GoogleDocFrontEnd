04e36678c356be8c9bb269e361d64fd8eb527251
CristianSalva <158475444+CristianSalva@users.noreply.github.com>
2024-12-02 09:16:54 -0400

    Added gitignore

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import TextEditor from "./TextEditor";
 
+import { v4 as uuidV4 } from "uuid";
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 function App() {
   return (
-    <>
-      <TextEditor />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to={`/documents/${uuidV4()}`} />} />
+        <Route path="/documents/:id" element={<TextEditor />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 export default App;
